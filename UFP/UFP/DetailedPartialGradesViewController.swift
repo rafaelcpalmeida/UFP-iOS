87b6a5d86b4814fbc5d5bfb7be987102df814563
Rafael Almeida <rafaelcpalmeida@gmail.com>
2017-04-15 01:42:39 +0100

    Implement detailed partial grades view

--- UFP/UFP/DetailedPartialGradesViewController.swift
+++ UFP/UFP/DetailedPartialGradesViewController.swift
@@ -12,12 +12,10 @@ import SwiftyJSON
 class DetailedPartialGradesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var gradesTable: UITableView!
+    @IBOutlet weak var navigationBar: UINavigationBar!
     
-    let apiController = APIController()
     let tableCellIdentifier = "tableCell"
-    
-    var headers = [String]()
-    var finalGrades = [String: [FinalGrades]]()
+    var partialGrades: PartialGrades?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,47 +23,27 @@ class DetailedPartialGradesViewController: UIViewController, UITableViewDataSour
         gradesTable.delegate = self
         gradesTable.dataSource = self
         
-        apiController.getUserFinalGrades(APICredentials.sharedInstance.apiToken!, completionHandler: { (json, error) in
-            if(json["status"] == "Ok") {
-                for (keyLevel, data) in json["message"] {
-                    for (keyCourse, _) in data {
-                        self.headers.append(keyLevel + " - " + keyCourse)
-                        
-                        var grades = [FinalGrades]()
-                        
-                        for (_, value) in json["message"][keyLevel][keyCourse] {
-                            //self.grades.append([keyLevel + " - " + keyCourse: FinalGrades(name: value["unidade"].stringValue, grade: value["nota"].stringValue)])
-                            grades.append(FinalGrades(name: value["unidade"].stringValue, grade: value["nota"].stringValue))
-                        }
-                        
-                        self.finalGrades[keyLevel + " - " + keyCourse] = grades
-                    }
-                }
-            } else {
-            }
-            
-            self.gradesTable.reloadData()
-        })
-        
+        if let grades = partialGrades {
+            self.navigationBar.topItem?.title = "\(grades.name) - Notas Parciais"
+        }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return headers.count
+        return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.finalGrades[headers[section]]!.count
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return headers[section]
+        return (partialGrades?.grades.count)!
     }
     
     internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: tableCellIdentifier, for: indexPath as IndexPath)
         
-        if let finalGradeDetails = self.finalGrades[headers[indexPath.section]]?[indexPath.row] {
-            cell.textLabel?.text = finalGradeDetails.name
+        if let grades = partialGrades {
+            let gradeDetails = Array(grades.grades)[indexPath.row]
+            
+            cell.textLabel?.text = gradeDetails.key
+            cell.detailTextLabel?.text = gradeDetails.value
         }
         
         return cell
