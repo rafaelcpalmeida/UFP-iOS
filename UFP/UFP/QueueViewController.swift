3dbf515cf97c43341d5744ebc01638068e9896ac
Rafael Almeida <rafaelcpalmeida@gmail.com>
2017-05-12 18:42:23 +0100

    Implement queue table scroll down to update

--- UFP/UFP/QueueViewController.swift
+++ UFP/UFP/QueueViewController.swift
@@ -19,31 +19,35 @@ class QueueTableViewCell: UITableViewCell {
 class QueueViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    @IBOutlet weak var gradesTable: UITableView!
+    @IBOutlet weak var queueTable: UITableView!
     
     let apiController = APIController()
     let tableCellIdentifier = "tableCell"
     
     var queueStatus = [Queue]()
     
+    lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(QueueViewController.handleRefresh(_:)), for: UIControlEvents.valueChanged)
+        
+        return refreshControl
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        gradesTable.delegate = self
-        gradesTable.dataSource = self
+        queueTable.delegate = self
+        queueTable.dataSource = self
         
-        apiController.getQueueStatus(completionHandler: { (json, error) in
-            self.activityIndicator.stopAnimating()
-            if(json["status"] == "Ok") {
-                for (keyLevel, data) in json["message"] {
-                    self.queueStatus.append(Queue(service: keyLevel, number: data["number"].stringValue, description: data["desc"].stringValue, waiting: data["waiting"].stringValue, lastUpdate: data["last_update"].stringValue))
-                }
-            } else {
-            }
-            
-            self.gradesTable.reloadData()
-        })
+        self.queueTable.addSubview(self.refreshControl)
         
+        fetchNewData()
+    }
+    
+    func handleRefresh(_ refreshControl: UIRefreshControl) {
+        self.queueStatus.removeAll()
+        
+        fetchNewData(updated: true)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -53,19 +57,39 @@ class QueueViewController: UIViewController, UITableViewDataSource, UITableViewD
     internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: tableCellIdentifier, for: indexPath as IndexPath) as! QueueTableViewCell
         
-        cell.descriptionLabel.text = String(format: NSLocalizedString("Service %@ - %@", comment: ""), self.queueStatus[indexPath.row].service, self.queueStatus[indexPath.row].description)
-        
-        cell.numberLabel.text = String(format: NSLocalizedString("Number %@", comment: ""), self.queueStatus[indexPath.row].number)
-        
-        if (Int(self.queueStatus[indexPath.row].waiting) == 0) {
-            cell.waitingLabel.isHidden = true
-        } else {
-            cell.waitingLabel.text = String(format: NSLocalizedString("%@ waiting", comment: ""), self.queueStatus[indexPath.row].waiting)
+        if self.queueStatus.indices.contains(indexPath.row) {
+            cell.descriptionLabel.text = String(format: NSLocalizedString("Service %@ - %@", comment: ""), self.queueStatus[indexPath.row].service, self.queueStatus[indexPath.row].description)
+            
+            cell.numberLabel.text = String(format: NSLocalizedString("Number %@", comment: ""), self.queueStatus[indexPath.row].number)
+            
+            if (Int(self.queueStatus[indexPath.row].waiting) == 0) {
+                cell.waitingLabel.isHidden = true
+            } else {
+                cell.waitingLabel.text = String(format: NSLocalizedString("%@ waiting", comment: ""), self.queueStatus[indexPath.row].waiting)
+            }
+            
+            cell.lastUpdateLabel.text = String(format: NSLocalizedString("Last update - %@", comment: ""), self.queueStatus[indexPath.row].lastUpdate)
         }
         
-        cell.lastUpdateLabel.text = String(format: NSLocalizedString("Last update - %@", comment: ""), self.queueStatus[indexPath.row].lastUpdate)
-        
         return cell
+    }
+    
+    func fetchNewData(updated: Bool = false) {
+        apiController.getQueueStatus(completionHandler: { (json, error) in
+            self.activityIndicator.stopAnimating()
+            if(json["status"] == "Ok") {
+                for (keyLevel, data) in json["message"] {
+                    self.queueStatus.append(Queue(service: keyLevel, number: data["number"].stringValue, description: data["desc"].stringValue, waiting: data["waiting"].stringValue, lastUpdate: data["last_update"].stringValue))
+                }
+            } else {
+            }
+            
+            self.queueTable.reloadData()
+            
+            if updated {
+                self.refreshControl.endRefreshing()
+            }
+        })
     }
     
 }
